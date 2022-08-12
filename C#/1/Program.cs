398f2d047e1d00f3f6ab0f3f00f82cb9d3b5977f
Aeshe <syslichek@mail.ru>
2022-08-12 22:49:40 +0300

    добавила задачки

---
+++ C#/1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Известны два расстояния: одно в километрах, другое — в футах (1 фут 0,305 м). Какое из расстояний меньше?
+int distanceKM = 1;
+int distanceF = 20000;
+int min = 0;
+double distanceM1 = distanceKM * 1000;
+double distanceM2 = distanceF * 0.305;
+
+if (distanceM1 < distanceM2)
+{ 
+    min = distanceKM;
+}
+else 
+{
+    min = distanceF;
+}
+
+Console.WriteLine(min);
---
+++ C#/2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Даны радиус круга и сторона квадрата. У какой фигуры площадь больше?
+int circleR = 6;
+int square = 10;
+double circleS = 3.141 * circleR * circleR;
+double squareS = square * square;
+
+if (circleS > squareS)
+{
+    Console.WriteLine("Площадь круга больше");
+}
+else if (circleS < squareS)
+{
+    Console.WriteLine("Площадь квадрата больше");
+}
+else 
+{
+    Console.WriteLine("Площади фигур равны");
+}
