6fa90118603563df6af29b7f7b6432abbbcc64fa
Aeshe <syslichek@mail.ru>
2022-08-14 15:05:59 +0300

    mod Turtle

--- C#/4-Turtle/Program.cs
+++ C#/4-Turtle/Program.cs
@@ -9,6 +9,7 @@ namespace _4_Turtle
 {
     class Program
     {
+        /* объявление функции lдля написания букв черепашкой
         static void DrawT (int height)
             {
             Turtle.Angle = 0; // угол 0, черепашка смотрит наверх
@@ -18,10 +19,32 @@ namespace _4_Turtle
             Turtle.Angle = 90; // угол 90
             Turtle.Move(40);
             }
+            */
+
         static void Main(string[] args) //место откуда стартует программа
         {
-            Turtle.Speed = 9;
+            GraphicsWindow.KeyDown += GraphicsWindow_KeyDown; //функция, которая реагирует на нажатие стрелок
+            Turtle.PenUp(); //не рисовать линию за черепашкой, penDown() - рисовать
+            
+            GraphicsWindow.BrushColor = "Red"; // цвет кисти
+            GraphicsWindow.BackgroundColor = "LightYellow"; //цвет фона
+            var eat = Shapes.AddRectangle(10, 10); //Рисуем фигуру еды
+            int x = 200;
+            int y = 200;
+            Shapes.Move(eat, x, y); // Положение фигуры еды
 
+            while (true)
+            {
+                Turtle.Move(10);
+                if (Turtle.X >= x && Turtle.X < x+10 && Turtle.Y >= y && Turtle.Y< y+10 ) //т.к. размер еды 10 на 10
+            {
+                x = new Random().Next(0, GraphicsWindow.Width);
+                y = new Random().Next(0, GraphicsWindow.Height);
+                Shapes.Move(eat, x, y);
+                Turtle.Speed++;
+            }
+
+            }
 
 
 
@@ -56,7 +79,7 @@ namespace _4_Turtle
             Turtle.Y = 200;
             DrawT(30); //рисуем Т большую
             */
-            
+
 
 
 
@@ -65,6 +88,25 @@ namespace _4_Turtle
             //   Turtle.Move(60);
             //    Turtle.Turn(60); //поворот на угол 60                              
             //}        
+        }
+        private static void GraphicsWindow_KeyDown()
+        {
+            if (GraphicsWindow.LastKey == "Up")
+            {
+                Turtle.Angle = 0;
+            }
+            else if (GraphicsWindow.LastKey == "Right")
+            {
+                Turtle.Angle = 90;
+            }
+            else if (GraphicsWindow.LastKey == "Down")
+            {
+                Turtle.Angle = 180;
+            }
+            else if (GraphicsWindow.LastKey == "Left")
+            {
+                Turtle.Angle = 270;
+            }
         }
     }
 }
