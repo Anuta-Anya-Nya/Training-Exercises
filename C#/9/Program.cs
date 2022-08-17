fec788ada165d5cdcc8b71514c71a70378c94696
Aeshe <syslichek@mail.ru>
2022-08-18 00:23:14 +0300

    Add ex9

---
+++ C#/9/Program.cs
@@ -0,0 +1,9 @@
+﻿string Derive(double coefficient, double exponent)
+  {
+    double mult = coefficient * exponent;
+    double numb2 = exponent - 1;
+    string result = mult.ToString() + "x^" + numb2.ToString();
+    return result;
+    // return $"{coefficient * exponent}x^{exponent - 1}";
+  }
+Console.WriteLine(Derive(7, 8));
