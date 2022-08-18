c24ac2091fc4c4da3651a2efae92711983eefeac
Aeshe <syslichek@mail.ru>
2022-08-18 15:41:53 +0300

    Add task10 and task11

---
+++ C#/10/Program.cs
@@ -0,0 +1,29 @@
+﻿// Given a string made up of letters a, b, and/or c, switch the position of letters a and b 
+//(change a to b and vice versa). Leave any incidence of c untouched.
+//Example:
+//'acb' --> 'bca'
+//'aabacbaa' --> 'bbabcabb'
+string Switcheroo(string x)
+{
+    /*string result = String.Empty;
+    for (int i = 0; i < x.Length; i++)
+    {
+        switch (x[i])
+        {
+            case 'a':
+                result = result + 'b';
+                break;
+            case 'b':
+                result = result + 'a';
+                break;
+            default:
+                result = result + x[i];
+                break;
+        }
+    }
+    return result;*/
+    return  x.Replace("a","B").Replace("b","a").ToLower(); //(что меняем, на что меняем)
+    
+    //return x.Replace("a","d").Replace("b","a").Replace("d","b");
+}
+Console.WriteLine(Switcheroo("abc"));
---
+++ C#/11/Program.cs
@@ -0,0 +1,29 @@
+﻿// you are given a string of space separated numbers, and have to return the highest and lowest number.
+// Kata.HighAndLow("1 2 3 4 5");  // return "5 1"
+string HighAndLow(string numbers)
+{
+// int[] arr = numbers.Split(' ').Select(Int32.Parse).ToArray(); // массив чисел из строки
+
+string[] arr = numbers.Split(' '); // массив строк
+int max = Convert.ToInt32(arr[0]);
+int min = Convert.ToInt32(arr[0]);
+for (int i=1; i<arr.Length; i++)
+{
+    if (max < Convert.ToInt32(arr[i])) 
+    {
+        max = Convert.ToInt32(arr[i]);  
+    }
+    if (min > Convert.ToInt32(arr[i]))
+    {
+        min = Convert.ToInt32(arr[i]);  
+    }
+}
+return $"{max} {min}";
+}
+Console.WriteLine(HighAndLow("5 0 8 45 12 -1 100"));
+
+string HighAndLow2(string numbers)
+  {
+    var listnums = numbers.Split(' ').Select(int.Parse); //коллекция 
+    return string.Format("{0} {1}", listnums.Max(), listnums.Min());
+  }
