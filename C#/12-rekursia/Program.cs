5a7f85554927392402dea494a1e85783047ebc4e
Aeshe <syslichek@mail.ru>
2022-09-02 00:33:47 +0300

    несколько задачек

--- C#/12-rekursia/Program.cs
+++ C#/12-rekursia/Program.cs
@@ -5,7 +5,7 @@ int Squaring(int n)
     if (n == 1) return 1;
     return Squaring(n - 1) + 2 * (n - 1) + 1; //3: Sq(2)+2*(3-1)+1; Sq(1)+2*(2-1)+ 4+1+1; 1+2+4+2=9
 }
-Console.WriteLine(Squaring(3));
+Console.WriteLine("Задача 1: " + Squaring(3));
 
 //Комбинаторная задача
 //Задача. Вычислить количество комбинаций из n разных элементов по m. Количество комбинаций определяется формулой 
@@ -16,7 +16,7 @@ int Combin(int n, int m)
     if (m > n && n >= 0) return 0;
     return Combin(n - 1, m - 1) + Combin(n - 1, m);
 }
-Console.WriteLine(Combin(6, 4));
+Console.WriteLine("Задача 2: " + Combin(6, 4));
 
 //Задача. Разработать рекурсивную функцию, которая реализует синтаксический анализатор для понятия идентификатор. 
 //Как известно, в языке программирования C# идентификатор подчиняется следующим правилам:
@@ -53,14 +53,65 @@ bool IsIdentifier(string id, int position)
         return true;
     }
 }
-Console.WriteLine(IsIdentifier("12a", 0));
+Console.WriteLine("Задача 3: " + IsIdentifier("12a", 0));
 
 // Задача. 
 //Составить программу, которая реализует синтаксический анализатор для понятия «expression»  -простое выражение
-// содержит буквы и только один знак операции + - *. Первый и последний символ - буква
+// содержит буквы и только один знак операции ПОДРЯД + - *. Первый и последний символ - буква
 
 // Для решения данной задачи нужно реализовать три функции:
 // функцию IsIdentifier(), которая определяет есть ли простой идентификатор буквой;
 // функцию IsOperation(), которая определяет ли есть операция синтаксически верной в соответствии с условием задачи;
 // рекурсивную функцию IsExpression(), которая определяет есть ли простое выражение синтаксически правильным 
 // в соответствии с условием задачи.
+bool ItIdentifier(char c)
+{
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) return true;
+    else return false;
+}
+
+bool ItOperation(char c)
+{
+    if (c == '+' || c == '-' || c == '*') return true;
+    else return false;
+}
+
+bool ItExpression(string expr, int position, bool isOp)
+{
+    if (position == 0)
+    {
+        char c = expr[position];
+        if (ItIdentifier(c))
+        {
+            return ItExpression(expr, position + 1, false);
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        char c = expr[position];
+        if (position < expr.Length - 1)
+        {
+            if (ItIdentifier(c))
+            {
+                return ItExpression(expr, position + 1, false); // false показывает следующей проверке что стояла буква. ЕВЛИ хотим в проверке только один знак +-* то прописать вместо false - ItOp
+            }
+            else if (ItOperation(c) && !isOp)
+            {
+                return ItExpression(expr, position + 1, true);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else //проверяем последний элемент
+        {
+            return ItIdentifier(c); // оптимизация кода, выведет true или false
+        }
+    }
+}
+Console.WriteLine("Задача 4: " + ItExpression("a+b*vb", 0, false));
